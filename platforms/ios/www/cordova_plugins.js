4dd8ec92781578c95b8178dfc00a7d21e26a993a
James Nebeker <jnebeker@radiusnetworks.com>
2015-03-28 10:59:35 -0400

    Re-add ios platform for new version of the plugin

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -13,20 +13,12 @@ module.exports = [
         "clobbers": [
             "cordova.logger"
         ]
-    },
-    {
-        "file": "plugins/com.radiusnetworks.cordova.proximitykit/www/proximitykit.js",
-        "id": "com.radiusnetworks.cordova.proximitykit.proximitykit",
-        "clobbers": [
-            "radiusnetworks.plugins.proximitykit"
-        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "org.apache.cordova.console": "0.2.9",
-    "com.radiusnetworks.cordova.proximitykit": "0.5.1"
+    "org.apache.cordova.console": "0.2.13"
 }
 // BOTTOM OF METADATA
 });
